8bc05e0cf5e8adfd757ecab8a3604ee2ec963e1b
Karan <karandayani39@gmail.com>
2023-09-27 18:27:09 +0530

    added api call

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ import "./style.css"
 import Navbar from "./components/Navbar";
 
 export default function App() {
+    const [charArr, setCharArr] = React.useState([])
+
+    console.log(charArr)
+
+    function characterChange(char) {
+        // const url = `https://gateway.marvel.com/v1/public/characters?ts=${process.env.REACT_APP_TS}&name=hulk&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH}#`
+        const url = `https://gateway.marvel.com/v1/public/characters?ts=1&name=${char}&apikey=c652e9239e342f5196a3b4aab84a7b78&hash=c0ffed02dfca96ca41999c3c585bad2f#`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setCharArr(data.data.results))
+    }
+
+    React.useEffect(() => {
+        characterChange("iron man");
+    },[])
+
     return (
         <div>
-            <Navbar />
+            <Navbar/>
         </div>
     )
 }
