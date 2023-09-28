9f081255cee8c318fe768c4f37936df025facaed
Karan <karandayani39@gmail.com>
2023-09-28 11:30:43 +0530

    added responsive styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function Main(props) {
     return (
-        <main>
+        <main className="d-flex justify-content-around align-items-center">
             <img className="main-img" src={`${props.charArr[0].thumbnail.path}.${props.charArr[0].thumbnail.extension}`} alt=""></img>
-            <h1 className="main-name">{props.charArr[0].name}</h1>
-            <p className="main-description">{props.charArr[0].description}</p>
+            <div className="main-info text-center">
+                <h1 className="main-name">{props.charArr[0].name}</h1>
+                <p className="main-description">{props.charArr[0].description}</p>
+            </div>
         </main>
     )
 }
